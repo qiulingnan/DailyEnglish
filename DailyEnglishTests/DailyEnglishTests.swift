967241a89b448fc2afc42150810d3ee578e1b7f8
邱岭男 <309557438@qq.com>
2018-10-29 14:37:16 +0800

    Initial Commit

---
+++ DailyEnglishTests/DailyEnglishTests.swift
@@ -0,0 +1,34 @@
+//
+//  DailyEnglishTests.swift
+//  DailyEnglishTests
+//
+//  Created by 邱岭男 on 2018/10/29.
+//  Copyright © 2018年 邱岭男. All rights reserved.
+//
+
+import XCTest
+@testable import DailyEnglish
+
+class DailyEnglishTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
